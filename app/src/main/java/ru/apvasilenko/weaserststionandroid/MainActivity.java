c6602ee0973936ebb905306aa8427d6b9c46846a
Антон Василенко <apwasilenko@gmail.com>
2024-02-27 20:52:37 +0700

    An update button and a response text field have been added.
    Добавлена кнопка обновления и текстовое поле ответа.

--- app/src/main/java/ru/apvasilenko/weaserststionandroid/MainActivity.java
+++ app/src/main/java/ru/apvasilenko/weaserststionandroid/MainActivity.java
@@ -2,12 +2,31 @@ package ru.apvasilenko.weaserststionandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btUpdate;
+    TextView tvResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+
+        btUpdate = (Button) findViewById ( R.id.btUpdate );
+        btUpdate.setOnClickListener ( this );
+
+        tvResponse = (TextView) findViewById ( R.id.tvResponse );
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId ()){
+            case R.id.btUpdate:
+                tvResponse.setText (  R.string.textTV2);
+        }
     }
 }
